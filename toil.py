ba7c853580a8c20dd6519d5c8293145ba3f87848
Kobayashi Takahiro <koba925@gmail.com>
2026-06-16 21:22:53 +0900

    Step: 0201_scan_numbers

--- toil.py
+++ toil.py
@@ -1,3 +1,39 @@
+class Scanner:
+    def __init__(self, src):
+        self._src = src
+        self._start_pos = 0
+        self._current_pos = 0
+        self._tokens = []
+
+    def tokenize(self):
+        while True:
+            self._start_pos = self._current_pos
+            match self._current_char():
+                case "$EOF":
+                    self._tokens.append("$EOF")
+                    break
+                case c if c.isdecimal(): self._number()
+                case invalid:
+                    assert False, f"Invalid character @ tokenize(): {invalid}"
+
+        return self._tokens
+
+    def _number(self):
+        while self._current_char().isdecimal(): self._advance()
+        self._tokens.append(int(self._lexeme()))
+
+    def _lexeme(self):
+        return self._src[self._start_pos:self._current_pos]
+
+    def _advance(self): self._current_pos += 1
+
+    def _current_char(self):
+        if self._current_pos < len(self._src):
+            return self._src[self._current_pos]
+        else:
+            return "$EOF"
+
+
 class Environment:
     def __init__(self, parent=None):
         self._parent = parent
@@ -98,6 +134,9 @@ class Interpreter:
 
         self._env = Environment(self._env)
 
+    def scan(self, src):
+        return Scanner(src).tokenize()
+
     def eval(self, expr):
         return Evaluator().eval(expr, self._env)
 
@@ -108,280 +147,12 @@ if __name__ == "__main__":
 
     # Example
 
-    print("Factorial:")
+    print("Scan numbers:")
 
-    def factorial_iter(n):
-        result = 1
-        while n > 0:
-            result = result * n
-            n = n - 1
-        return result
-    print(factorial_iter(0))
-    # -> 1
-    print(factorial_iter(1))
-    # -> 1
-    print(factorial_iter(4))
-    # -> 24
+    print(toil.scan(r""""""))  # -> ['$EOF']
+    print(toil.scan(r"""2"""))  # -> [2, '$EOF']
+    print(toil.scan(r"""02"""))  # -> [2, '$EOF']
+    print(toil.scan(r"""23"""))  # -> [23, '$EOF']
 
-    toil.eval(("define", ["factorial_iter", ("func", [["n"], ("seq", [
-            ("define", ["result", 1]),
-            ("while", [("greater", ["n", 0]), ("seq", [
-                ("assign", ["result", ("mul", ["result", "n"])]),
-                ("assign", ["n", ("sub", ["n", 1])]),
-            ])]),
-            "result"
-    ])])]))
-    print(toil.eval(("factorial_iter", [0])))
-    # -> 1
-    print(toil.eval(("factorial_iter", [1])))
-    # -> 1
-    print(toil.eval(("factorial_iter", [4])))
-    # -> 24
-
-    factorial_rec = lambda n: 1 if n == 0 else n * factorial_rec(n - 1)
-    print(factorial_rec(0))
-    # -> 1
-    print(factorial_rec(1))
-    # -> 1
-    print(factorial_rec(4))
-    # -> 24
-
-    toil.eval(("define", ["factorial_rec", ("func", [["n"],
-        ("if", [("equal", ["n", 0]),
-            1,
-            ("mul", ["n", ("factorial_rec", [("sub", ["n", 1])])])
-        ])
-    ])]))
-    print(toil.eval(("factorial_rec", [0])))
-    # -> 1
-    print(toil.eval(("factorial_rec", [1])))
-    # -> 1
-    print(toil.eval(("factorial_rec", [4])))
-    # -> 24
-
-    print("Fibonacci:")
-
-    def fib_iter(n):
-        a = 0; b = 1
-        while n > 0:
-            tmp = b; b = a + b; a = tmp
-            n = n - 1
-        return a
-    print(fib_iter(0))
-    # -> 0
-    print(fib_iter(1))
-    # -> 1
-    print(fib_iter(6))
-    # -> 8
-
-    toil.eval(("define", ["fib_iter", ("func", [["n"], ("seq", [
-        ("define", ["a", 0]),
-        ("define", ["b", 1]),
-        ("while", [("greater", ["n", 0]), ("seq", [
-            ("define", ["tmp", "b"]),
-            ("assign", ["b", ("add", ["a", "b"])]),
-            ("assign", ["a", "tmp"]),
-            ("assign", ["n", ("sub", ["n", 1])])
-        ])]),
-        "a"
-    ])])]))
-    print(toil.eval(("fib_iter", [0])))
-    # -> 0
-    print(toil.eval(("fib_iter", [1])))
-    # -> 1
-    print(toil.eval(("fib_iter", [6])))
-    # -> 8
-
-    fib_rec = lambda n: 0 if n == 0 else 1 if n == 1 else fib_rec(n - 1) + fib_rec(n - 2)
-    print(fib_rec(0))
-    # -> 0
-    print(fib_rec(1))
-    # -> 1
-    print(fib_rec(6))
-    # -> 8
-
-    toil.eval(("define", ["fib_rec", ("func", [["n"],
-        ("if", [("equal", ["n", 0]), 0,
-        ("if", [("equal", ["n", 1]), 1,
-        ("add", [
-            ("fib_rec", [("sub", ["n", 1])]),
-            ("fib_rec", [("sub", ["n", 2])])
-        ])])])
-    ])]))
-    print(toil.eval(("fib_rec", [0])))
-    # -> 0
-    print(toil.eval(("fib_rec", [1])))
-    # -> 1
-    print(toil.eval(("fib_rec", [6])))
-    # -> 8
-
-    print("GCD:")
-
-    def gcd_iter(a, b):
-        while b > 0: tmp = b; b = a % b; a = tmp
-        return a
-    print(gcd_iter(12, 18))
-    # -> 6
-
-    toil.eval(("define", ["gcd_iter", ("func", [["a", "b"],
-        ("while", [("greater", ["b", 0]), ("seq", [
-            ("define", ["tmp", "b"]),
-            ("assign", ["b", ("mod", ["a", "b"])]),
-            ("assign", ["a", "tmp"])
-        ])])
-    ])]))
-    print(toil.eval(("gcd_iter", [12, 18])))
-    # -> 6
-
-    gcd_rec = lambda a, b: a if b == 0 else gcd_rec(b, a % b)
-    print(gcd_rec(12, 18))
-    # -> 6
-
-    toil.eval(("define", ["gcd_rec", ("func", [["a", "b"],
-        ("if", [("equal", ["b", 0]),
-            "a",
-            ("gcd_rec", ["b", ("mod", ["a", "b"])])
-        ])
-    ])]))
-    print(toil.eval(("gcd_rec", [12, 18])))
-    # -> 6
-
-    print("Even/Odd (Mutual Recursion):")
-
-    even = lambda n: True if n == 0 else odd(n - 1)
-    odd = lambda n: False if n == 0 else even(n - 1)
-    print(even(2))
-    # -> True
-    print(even(3))
-    # -> False
-    print(odd(2))
-    # -> False
-    print(odd(3))
-    # -> True
-
-    toil.eval(("define", ["even", ("func", [["n"],
-        ("if", [("equal", ["n", 0]), True, ("odd", [("sub", ["n", 1])])])
-    ])]))
-    toil.eval(("define", ["odd", ("func", [["n"],
-        ("if", [("equal", ["n", 0]), False, ("even", [("sub", ["n", 1])])])
-    ])]))
-    print(toil.eval(("even", [2])))
-    # -> True
-    print(toil.eval(("even", [3])))
-    # -> False
-    print(toil.eval(("odd", [2])))
-    # -> False
-    print(toil.eval(("odd", [3])))
-    # -> True
-
-    print("Counter (Closure):")
-
-    def make_counter():
-        count = 0
-        def counter():
-            nonlocal count
-            count = count + 1
-            return count
-        return counter
-    c1 = make_counter()
-    c2 = make_counter()
-    print(c1())
-    # -> 1
-    print(c1())
-    # -> 2
-    print(c2())
-    # -> 1
-    print(c2())
-    # -> 2
-
-    toil.eval(("define", ["make_counter", ("func", [[], ("seq", [
-        ("define", ["count", 0]),
-        ("func", [[], ("assign", ["count", ("add", ["count", 1])])])
-    ])])]))
-    toil.eval(("define", ["c1", ("make_counter", [])]))
-    toil.eval(("define", ["c2", ("make_counter", [])]))
-    print(toil.eval(("c1", [])))
-    # -> 1
-    print(toil.eval(("c1", [])))
-    # -> 2
-    print(toil.eval(("c2", [])))
-    # -> 1
-    print(toil.eval(("c2", [])))
-    # -> 2
-
-    print("Binary search tree:")
-    print("Building tree:")
-
-    toil.eval(("define", ["node", ("func", [["val", "left", "right"], ("seq", [
-        ("func", [["op"],
-            ("if", [("equal", ["op", 1]), "val",
-            ("if", [("equal", ["op", 2]), "left",
-            "right"])])
-        ])
-    ])])]))
-
-    toil.eval(("define", ["n1", ("node", [2, 3, 4])]))
-    print(toil.eval(("n1", [1])))
-    print(toil.eval(("n1", [2])))
-    print(toil.eval(("n1", [3])))
-    # -> 2\n3\n4
-
-    toil.eval(("define", ["bst_put", ("func", [["bst", "val"],
-        ("if", [("equal", ["bst", None]),
-            ("node", ["val", None, None]),
-            ("seq", [
-                ("define", ["cur_val", ("bst", [1])]),
-                ("if", [("equal", ["val", "cur_val"]),
-                    "bst",
-                ("if", [("less", ["val", "cur_val"]),
-                    ("node", ["cur_val", ("bst_put", [("bst", [2]), "val"]), ("bst", [3])]),
-                    ("node", ["cur_val", ("bst", [2]), ("bst_put", [("bst", [3]), "val"])])
-                ])])
-            ])
-        ])
-    ])]))
-    toil.eval(("define",["bst", None]))
-    toil.eval(("define",["bst", ("bst_put", ["bst", 7])]))
-    toil.eval(("define",["bst", ("bst_put", ["bst", 3])]))
-    toil.eval(("define",["bst", ("bst_put", ["bst", 1])]))
-    toil.eval(("define",["bst", ("bst_put", ["bst", 9])]))
-    toil.eval(("define",["bst", ("bst_put", ["bst", 5])]))
-
-    print("Walking tree:")
-    toil.eval(("define", ["bst_walk", ("func", [["bst"],
-        ("if", [("equal", ["bst", None]),
-            None,
-            ("seq", [
-                ("bst_walk", [("bst", [2])]),
-                ("print", [("bst", [1])]),
-                ("bst_walk", [("bst", [3])]),
-            ])
-        ])
-    ])]))
-    toil.eval(("define", ["bst_find", ("func", [["bst", "val"],
-        ("if", [("equal", ["bst", None]),
-            False,
-            ("seq", [
-                ("define", ["cur_val", ("bst", [1])]),
-                ("if", [("equal", ["val", "cur_val"]),
-                    "val",
-                ("if", [("less", ["val", "cur_val"]),
-                    ("bst_find", [("bst", [2]), "val"]),
-                    ("bst_find", [("bst", [3]), "val"])
-                ])])
-            ])
-        ])
-    ])]))
-
-    toil.eval(("bst_walk",["bst"]))
-    # -> 1\n3\n5\n7\n9
-
-    print("Finding values:")
-    toil.eval(("seq", [
-        ("define", ["i", 0]),
-        ("while", [("less", ["i", 10]), ("seq", [
-            ("print", [("bst_find",["bst", "i"])]),
-            ("assign", ["i", ("add", ["i", 1])])
-        ])])
-    ]))
-    # -> False\n1\nFalse\n3\nFalse\n5\nFalse\n7\nFalse\n9
+    # print(toil.scan(r"""$"""))  # -> Invalid character
+    # print(toil.scan(r"""2$"""))  # -> Invalid character
